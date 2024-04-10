0f26ac46e2a3a2b2bba0252a0178505176211a86
Hideaki Kawai <kayhide@gmail.com>
2024-04-10 16:26:06 +0900

    Update clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -2,27 +2,23 @@ use std::fmt;
 
 #[derive(PartialEq, Eq, Debug)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let (h, m) = (minutes / 60, minutes % 60);
-        let (h, m) = if m < 0 { (h - 1, m + 60) } else { (h, m) };
         Clock {
-            hours: ((hours + h) % 24 + 24) % 24,
-            minutes: m,
+            minutes: (hours * 60 + minutes).rem_euclid(24 * 60),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Self::new(self.hours, self.minutes + minutes)
+        Self::new(0, self.minutes + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(f, "{:02}:{:02}", self.minutes / 60, self.minutes % 60)
     }
 }
